c80c9d7a81be11e3fe2f8f76c505090dd28a1feb
Ivo Majic <imajic@gmail.com>
2016-03-02 21:15:11 +0100

    Initial commit.

---
+++ src/main/java/moneyhero/MoneyheroApplication.java
@@ -0,0 +1,12 @@
+package moneyhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyheroApplication.class, args);
+	}
+}
---
+++ src/test/java/moneyhero/MoneyheroApplicationTests.java
@@ -0,0 +1,18 @@
+package moneyhero;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MoneyheroApplication.class)
+@WebAppConfiguration
+public class MoneyheroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
