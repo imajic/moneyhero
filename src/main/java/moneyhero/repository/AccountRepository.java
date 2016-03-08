930b24f8f259d42f72d2e6e0375dc961a29d4a6b
Ivo Majic <ivo.majic@soe.hr>
2016-03-08 16:38:29 +0100

    Broken commit. Added accounts tree.

--- src/main/java/moneyhero/repository/AccountRepository.java
+++ src/main/java/moneyhero/repository/AccountRepository.java
@@ -1,11 +1,13 @@
 package moneyhero.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import moneyhero.entity.Account;
 
-public interface AccountRepository extends CrudRepository<Account, UUID>{
-	
+public interface AccountRepository extends Repository<Account, UUID>{
+	List<Account> findAllByOrderByDesignationAsc();
 }
--- src/main/java/moneyhero/ui/views/NewTransactionView.java
+++ src/main/java/moneyhero/ui/views/NewTransactionView.java
@@ -1,26 +1,75 @@
 package moneyhero.ui.views;
 
+import java.util.ArrayList;
+
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.data.Container;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.HierarchicalContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
+
+import java.util.List;
+import moneyhero.entity.Account;
+import moneyhero.repository.AccountRepository;
 
 
 @SpringView(name = NewTransactionView.VIEW_NAME)
 public class NewTransactionView extends VerticalLayout implements View{
 	public static final String VIEW_NAME = "newTransactionView";
 	
+	@Autowired
+	private AccountRepository accountRepository;
+	
 	@PostConstruct
 	void init() {
 	    addComponent(new Label("This is the default view"));
+	    
+	    Tree tree = new Tree("Accounts", accountsContainer());
+	   
+	    addComponent(tree);
 	}
 	
 	@Override
 	public void enter(ViewChangeEvent event) {
 	    // the view is constructed in the init() method()
+	}
+	
+	private Container accountsContainer(){
+		HierarchicalContainer result = new HierarchicalContainer();
+		result.addContainerProperty("name", String.class, "");
+		
+		List<Account> ancestors = new ArrayList<>();
+		for (Account account : accountRepository.findAllByOrderByDesignationAsc()){
+			Item item = result.addItem(account.getUuid());
+			item.getItemProperty("name").setValue(account.getDescription());
+			
+			Account ancestor = null;
+			for (int i = ancestors.size() - 1; i > 0; i--){
+				if ( ! isAncestor(ancestors.get(i).getDesignation(), account.getDesignation())){
+					ancestors.remove(i);
+				}
+				else {
+					ancestor = ancestors.get(i);
+					break;
+				}
+			}
+			
+			result.setParent(account.getUuid(), ancestor.getUuid());
+		}
+		return result;
+	}
+	
+	private boolean isAncestor(long designation, long toDesignation){
+		// ORed digits 
+		return (designation | toDesignation) == toDesignation;
 	}
 }
 
