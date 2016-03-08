6ad81f4da8d1358a412198c50f57d38329b28bf0
Ivo Majic <ivo.majic@soe.hr>
2016-03-08 11:08:52 +0100

    Added db configuration and initial database script.

--- src/main/java/moneyhero/MoneyheroApplication.java
+++ src/main/java/moneyhero/MoneyheroApplication.java
@@ -1,10 +1,52 @@
 package moneyhero;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
 public class MoneyheroApplication {
+	
+  @Value("${db.username}")
+  private String user;
+
+  @Value("${db.password}")
+  private String password;
+
+  @Value("${db.url}")
+  private String dataSourceUrl;
+
+  @Value("${db.driver}")
+  private String driverClassName;
+
+  @Value("${db.connectionTimeoutMs}")
+  private int connectionTimeoutMs;
+
+  @Value("${db.connectionMaxLifeTimeMs}")
+  private int connectionMaxLifetimeMs;
+
+
+  // Data Source configuration
+  @Bean
+  public DataSource primaryDataSource() {
+      HikariConfig hc = new HikariConfig();
+      hc.setDriverClassName(driverClassName);
+      hc.setJdbcUrl(dataSourceUrl);
+      hc.setUsername(user);
+      hc.setPassword(password);
+      hc.setConnectionTestQuery("select 1 from dual");
+      hc.setConnectionTimeout(connectionTimeoutMs);
+      hc.setMaxLifetime(connectionMaxLifetimeMs);
+
+      HikariDataSource ds = new HikariDataSource(hc);
+      return ds;
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyheroApplication.class, args);
--- src/main/resources/db/migration/V0_1__Initial database.sql
+++ src/main/resources/db/migration/V0_1__Initial database.sql
@@ -1,94 +1,30 @@
-CREATE TABLE external_entry(
+CREATE TABLE account(
   uuid BINARY(16) NOT NULL,
-  entry_type VARCHAR(10) NOT NULL,
-  external_source VARCHAR(10) NOT NULL,
-  external_source_id VARCHAR(20),
-  created_by VARCHAR(20) NOT NULL,
-  creation_timestamp DATETIME NOT NULL,
-  last_modified_by VARCHAR(20) NOT NULL,
-  last_modification_timestamp DATETIME NOT NULL,
+  designation NUMERIC(7) NOT NULL,
+  description VARCHAR(255),
   version INTEGER NOT NULL,
-  PRIMARY KEY (uuid),
-  UNIQUE INDEX external_id_index(entry_type, external_source, external_source_id)
+  PRIMARY KEY (uuid)
 );
 
-CREATE TABLE external_entry_underyling(
+CREATE TABLE account_amount(
   uuid BINARY(16) NOT NULL,
-  issuer BINARY(16) NOT NULL,
-  PRIMARY KEY (uuid),
-  FOREIGN KEY uuid_fk_external_entry_uuid(uuid) REFERENCES external_entry(uuid),
-  FOREIGN KEY issuer_fk_external_entry(issuer) REFERENCES external_entry(uuid)
+  currency CHAR(3) NOT NULL,
+  amount NUMERIC(20) NOT NULL,
+  PRIMARY KEY (uuid, currency),
+  FOREIGN KEY uuid_fk_account(uuid) REFERENCES account(uuid)
 );
 
-CREATE TABLE external_entry_underlying_exchange(
+CREATE TABLE transaction(
   uuid BINARY(16) NOT NULL,
-  underlying BINARY(16) NOT NULL,
-  exchange BINARY(16) NOT NULL,
+  transaction_timestamp DATETIME NOT NULL,
+  description VARCHAR(255),
+  currency CHAR(3) NOT NULL,
+  amount NUMERIC(20) NOT NULL,
+  credits BINARY(16) NOT NULL,
+  debits BINARY(16) NOT NULL,
   PRIMARY KEY (uuid),
-  FOREIGN KEY uuid_fk_external_entry_uuid(uuid) REFERENCES external_entry(uuid),
-  FOREIGN KEY underyling_fk_external_entry_underlying(underyling) REFERENCES external_entry(uuid),
-  FOREIGN KEY exchange_fk_external_entry_exchange(exchange) REFERENCES external_entry(uuid)
+  FOREIGN KEY credits_fk_account(credits) REFERENCES account(uuid),
+  FOREIGN KEY debits_fk_account(debits) REFERENCES account(uuid),
+  INDEX transaction_timestamp_index(transaction_timestamp)
 );
 
-CREATE TABLE external_entry_history (
-  uuid BINARY(16) NOT NULL,
-  external_entry BINARY(16) NOT NULL,
-  entry_type VARCHAR(10) NOT NULL,
-  valid_from DATETIME NOT NULL,
-  valid_to DATETIME DEFAULT NULL,
-  created_by VARCHAR(20) NOT NULL,
-  creation_timestamp DATETIME NOT NULL,
-  last_time_seen DATETIME NOT NULL,
-  version INT NOT NULL,
-  PRIMARY KEY (uuid),
-  INDEX external_entry_history_chrono_index(external_entry, valid_from),
-  FOREIGN KEY external_entry_history_fk_external_entry(external_entry) REFERENCES external_entry(uuid)
-);
-
-CREATE TABLE external_entry_history_issuer (
-  external_entry_history BINARY(16) NOT NULL,
-  status CHAR(1) NOT NULL,
-  issuer_name VARCHAR(70) NOT NULL,
-  issuer_country CHAR(2),
-  raw_data TEXT,
-  PRIMARY KEY (external_entry_history),
-  FOREIGN KEY external_entry_history_issuer_fk_external_entry_history(external_entry_history) REFERENCES external_entry_history(uuid)
-);
-
-CREATE TABLE external_entry_history_equity (
-  external_entry_history BINARY(16) NOT NULL,
-  status CHAR(1) NOT NULL,
-  isin CHAR(12) NULL,
-  cusip CHAR(9) NULL,
-  shares_outstanding NUMERIC(12,5) NULL,
-  primary_exchange_code CHAR(6) NULL,
-  currency_code CHAR(3) NULL,
-  pair_value NUMERIC(14,5) NULL,
-  no_pair_value BOOLEAN NULL,
-  raw_data TEXT,
-  PRIMARY KEY (external_entry_history),
-  FOREIGN KEY external_entry_history_equity_fk_external_entry_history(external_entry_history) REFERENCES external_entry_history(uuid)
-);
-
-CREATE TABLE external_entry_history_exchange (
-  external_entry_history BINARY(16) NOT NULL,
-  status CHAR(1) NOT NULL,
-  exchange_name VARCHAR(70) NULL,
-  mic CHAR(4) NULL,
-  country_code CHAR(2) NULL,
-  raw_data TEXT,
-  PRIMARY KEY (external_entry_history),
-  FOREIGN KEY external_entry_history_exchange_fk_external_entry_history(external_entry_history) REFERENCES external_entry_history(uuid)
-);
-
-CREATE TABLE external_entry_history_underlying_exchange (
-  external_entry_history BINARY(16) NOT NULL,
-  status CHAR(1) NULL,
-  list_status CHAR(1) NULL,
-  local_code VARCHAR(50) NULL,
-  min_trading_quantity NUMBER(11) NULL,
-  list_date DATE NULL,
-  raw_data TEXT,
-  PRIMARY KEY (external_entry_history),
-  FOREIGN KEY external_entry_history_fk_external_entry_history(external_entry_history) REFERENCES external_entry_history(uuid)
-);
